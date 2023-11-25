f0462830df21241358f7ea7c6bd4d38fbefdddb9
Marius Grigaliūnas <77457402+marius-grigaliunas@users.noreply.github.com>
2023-11-25 23:41:26 +0100

    Update palindrome-checker.js
    added comments on my code

--- palindrome-checker/palindrome-checker.js
+++ palindrome-checker/palindrome-checker.js
@@ -1,16 +1,16 @@
 function palindrome(str) {
-  let word = str.toLowerCase();
+  let word = str.toLowerCase(); 
 
-  let result = word.replace(/(\W)|_/g, '');
+  let result = word.replace(/(\W)|_/g, '');  // Converting the give string to lower case and removing any characters that aren't digits or letters.
   let resultCounter = 0;
 
-  for(let i = 0; i < Math.trunc(result.length / 2); i++) {
-    if(result[i] == result[result.length - 1 - i]) {
+  for(let i = 0; i < Math.trunc(result.length / 2); i++) { // this algorithm checks the first char with the last, second char with the second to last and so on. 
+    if(result[i] == result[result.length - 1 - i]) {       // until they meet in the middle. if the characters match the result counter is being added to.
       resultCounter++;
     }
   }
 
-  if(resultCounter == Math.trunc(result.length / 2))
+  if(resultCounter == Math.trunc(result.length / 2))  // If the matched characters are equal to half of all the characters it means the word is a palindrome.
     return true;
   else
     return false;
